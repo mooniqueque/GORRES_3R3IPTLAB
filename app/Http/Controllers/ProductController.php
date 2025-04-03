a86b688fdfb0d405ae8433c27b07ac6a9b3474b0
Andreanne Monique Gorres <amygorres@gmail.com>
2025-04-03 11:52:02 +0800

    picturesfor,delete,update,andput

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -23,13 +23,16 @@ class ProductController extends Controller
         $request->validate([
             'name' => 'required|string|max:255',
             'description' => 'nullable|string',
-            'price' => 'required|numeric|min:0',
+            'price' => 'required|numeric',
             'quantity' => 'required|integer|min:0',
         ]);
 
         $product = Product::create($request->all());
 
-        return response()->json($product, 201);
+        return response()->json([
+            'message' => 'Product created successfully!',
+            'product' => $product
+        ], 201);
     }
 
     /**
